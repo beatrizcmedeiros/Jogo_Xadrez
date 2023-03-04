ebb65ce04f3868d04af7e7623884bc8ead4ce5c6
Beatriz Medeiros <beatrizcsmedeiros@gmail.com>
2023-03-04 11:40:14 -0300

    Começando a implementação do tabuleiro e das peças.

--- src/tabuleirodejogo/Posicao.java
+++ src/tabuleirodejogo/Posicao.java
@@ -1,21 +1,12 @@
-package tabuleiro;
+package tabuleirodejogo;
 
 public class Posicao {
-	private int coluna;
 	private int linha;
+	private int coluna;
 	
 	public Posicao(int coluna, int linha) {
-		super();
 		this.coluna = coluna;
 		this.linha = linha;
-	}
-
-	public int getColuna() {
-		return coluna;
-	}
-
-	public void setColuna(int coluna) {
-		this.coluna = coluna;
 	}
 
 	public int getLinha() {
@@ -25,7 +16,15 @@ public class Posicao {
 	public void setLinha(int linha) {
 		this.linha = linha;
 	}
+	
+	public int getColuna() {
+		return coluna;
+	}
 
+	public void setColuna(int coluna) {
+		this.coluna = coluna;
+	}
+	
 	@Override
 	public String toString() {
 		return String.format("%d , %d", coluna, linha);
---
+++ src/tabuleirodejogo/Tabuleiro.java
@@ -0,0 +1,29 @@
+package tabuleirodejogo;
+
+public class Tabuleiro {
+	private int linhas;
+	private int colunas;
+	private Peca[][] pecas;
+	
+	public Tabuleiro(int colunas, int linhas) {
+		this.colunas = colunas;
+		this.linhas = linhas;
+		pecas = new Peca[linhas][colunas];
+	}
+
+	public int getLinhas() {
+		return linhas;
+	}
+
+	public void setLinhas(int linhas) {
+		this.linhas = linhas;
+	}
+
+	public int getColunas() {
+		return colunas;
+	}
+
+	public void setColunas(int colunas) {
+		this.colunas = colunas;
+	}
+}// class Tabuleiro
