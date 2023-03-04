d4e4917022e5f7c0db92bed0633f4347874a72fd
Beatriz Medeiros <beatrizcsmedeiros@gmail.com>
2023-03-04 11:26:07 -0300

    Primeira classe: posição.

---
+++ src/tabuleiro/Posicao.java
@@ -0,0 +1,33 @@
+package tabuleiro;
+
+public class Posicao {
+	private int coluna;
+	private int linha;
+	
+	public Posicao(int coluna, int linha) {
+		super();
+		this.coluna = coluna;
+		this.linha = linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+
+	public void setColuna(int coluna) {
+		this.coluna = coluna;
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public void setLinha(int linha) {
+		this.linha = linha;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%d , %d", coluna, linha);
+	}
+}//class Posicao
