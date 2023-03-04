d6e05ef68c44d2b8b6748245839d4a49fe7bbef4
Beatriz Medeiros <beatrizcsmedeiros@gmail.com>
2023-03-04 12:08:46 -0300

    Camadas do xadrez e print do tabuleiro.

---
+++ src/xadrez/PecaXadrez.java
@@ -0,0 +1,17 @@
+package xadrez;
+
+import tabuleirodejogo.Peca;
+import tabuleirodejogo.Tabuleiro;
+
+public class PecaXadrez extends Peca{
+	private Color color;
+
+	public PecaXadrez(Tabuleiro tabuleiro, Color color) {
+		super(tabuleiro);
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}	
+}//class PecaXadrez
